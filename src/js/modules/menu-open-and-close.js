4543fb16bf98ca7e0ad006afc154957b38c705b7
fkazu <ketuago.kazu@outlook.jp>
2021-03-06 16:28:31 +0000

    メニューの開閉機能を実装するモジュールを追加し、取り込む。#3

---
+++ src/js/modules/menu-open-and-close.js
@@ -0,0 +1,44 @@
+//メニューボタン
+const menuButton = document.getElementById('menu-button');
+//メニューボタンのg要素
+const menuButtonG = menuButton.querySelector('svg > g');
+//ヘッダー要素
+const header = document.getElementById('header');
+//フラグ
+let flag = false;
+
+
+//メニューボタンのg要素にクリックイベントを追加。
+menuButtonG.addEventListener( 'click', () => {
+  if ( flag ) passive();
+  else active();
+}, { passive: true } );
+
+
+//アクティブ
+function active() {
+  menuButton.classList.add('-active');
+  header.classList.add('-active');
+  flag = true;
+
+  //windowにリサイズイベントを追加
+  window.addEventListener( 'resize', observe, { passive: true } );
+}
+
+
+//パッシブ
+function passive() {
+  menuButton.classList.remove('-active');
+  header.classList.remove('-active');
+  flag = false;
+
+  //windowのリサイズイベントを削除
+  window.removeEventListener( 'resize', observe );
+}
+
+
+// 監視
+function observe() {
+  //画面の幅がTC幅を越えたらパッシブ状態にする
+  if ( window.innerWidth >= PC_BREAKPOINT ) passive();
+}
